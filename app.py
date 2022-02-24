60d2a0e0e8f41142366506af3bf00ab7e85ac8cf
evanesoteric <29648607+evanesoteric@users.noreply.github.com>
2022-02-23 20:52:09 -0500

    Initialize

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ app = Flask(__name__, template_folder='templates', static_folder='static', stati
 def index():
     github_app = "https://github.com/evanesoteric/nordvpn-geolocator"
     github_website = "https://github.com/evanesoteric/nordvpn-geolocator-website"
-    last_updated_url = requests.get('https://raw.githubusercontent.com/evanesoteric/nordvpn-geolocate/main/last_updated.txt')
+    last_updated_url = requests.get('https://raw.githubusercontent.com/evanesoteric/nordvpn-geolocator/main/last_updated.txt')
     last_updated = last_updated_url.text
     if last_updated_url.status_code == 404:
         last_updated = "Error loading"
-    vpns_url = requests.get('https://raw.githubusercontent.com/evanesoteric/nordvpn-geolocate/main/vpns.json')
+    vpns_url = requests.get('https://raw.githubusercontent.com/evanesoteric/nordvpn-geolocator/main/vpns.json')
     vpns = json.loads(vpns_url.text)
     return render_template('index.html', vpns=vpns, github_app=github_app, github_website=github_website, last_updated=last_updated)
 
